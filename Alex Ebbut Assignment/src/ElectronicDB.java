4a54f9d8c9633920f0500364eccf1d829683c8f2
Beren Wallis <32566086+BerenWallis@users.noreply.github.com>
2018-05-08 15:16:38 +0100

    Update ElectronicDB.java

--- Alex Ebbut Assignment/src/ElectronicDB.java
+++ Alex Ebbut Assignment/src/ElectronicDB.java
@@ -14,11 +14,30 @@ public class ElectronicDB {
 		 //Start CLI - Command Line Interface
 		 while(!exit)
 		 {
-		 
-		 //Get Input Here --
-		 
-		 
-		 //Add files to DB with Add
+			System.out.println("What do you want to do? [A]dd to Database, [E]xit");
+            		Scanner inputScanner = new Scanner(System.in);
+            		String input = inputScanner.nextLine();
+            		switch (input.toUpperCase()) {
+                		case "A":
+					System.out.println("Enter Key");
+					String Key = inputScanner.nextLine();
+					System.out.println("Enter Name");
+					String Name = inputScanner.nextLine();
+					System.out.println("Enter Date");
+					String Date = inputScanner.nextLine();
+					System.out.println("Enter Amount Of Copies");
+					String Copies = inputScanner.nextLine();
+					System.out.println("Enter Runtime In Mins");
+					String Runtime = inputScanner.nextLine();
+					System.out.println("Enter Score");
+					String Score = inputScanner.nextLine();
+                    			BluerayDB.add(Key,Name,Date,Copies,Runtime,Score);
+                   		 	break;
+                		case "E":
+                    			exit = true; 
+                		default:
+                   		 	break; //invalid input does nothing - loops again
+            }
 		 
 		 }
 		 //End CLI
