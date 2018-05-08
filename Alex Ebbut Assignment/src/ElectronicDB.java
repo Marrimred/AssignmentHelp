309073978a3c41bc54dd915d24990e51cfde1a66
Beren Wallis <32566086+BerenWallis@users.noreply.github.com>
2018-05-08 14:52:09 +0100

    Update ElectronicDB.java

--- Alex Ebbut Assignment/src/ElectronicDB.java
+++ Alex Ebbut Assignment/src/ElectronicDB.java
@@ -6,6 +6,7 @@ public class ElectronicDB {
 		 long startTime = System.nanoTime();
 		 System.out.println("Starting DB...");
 		 Database BluerayDB = new Database();
+		 BluerayDB.Add("GG13","BOOBIES","01/10/97",3,160,9)
 		 long endTime   = System.nanoTime();
 		 System.out.println("DB took " + (Math.round((endTime - startTime) / 100000.0)/10000.0) + " seconds to open");
 		 System.out.println();
